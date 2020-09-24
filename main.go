1f93d3ea42f05de3226b99041b26f3b889006cbd
Orim Dominic <orimdominic@gmail.com>
2020-09-24 16:42:49 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+)
+
+func main() {
+
+}
+
+/*
+	Algorithm
+*	print help/instructions
+-	Get interval in secs from user or set to default of 5s
+-	Get server url
+-	start interval timer
+-	make http request
+-	obtain status code
+-	determine if it is error or not
+-	set values accordingly (checks, up, down)
+-	push output to array (check time, status)
+-	log output to console (checks, ups, downs, currentStatus)
+-	on exit or interrupt, write array to disk, with details
+*/
+
+//  How do I record screeens on linux
+// Remember to add image to readmes
+// Add readme issue, pr and image to go-mmm
+// Draft go-mmm article
+// go-server-monitor app
